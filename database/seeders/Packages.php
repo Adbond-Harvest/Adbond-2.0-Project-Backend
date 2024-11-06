fba46c10fb71c9e9006de7a879db22dd3b6bdf7e
princeak <akalodave@gmail.com>
2024-11-06 14:52:53 +0100

    modifying promo_products to fix migration issue

--- database/seeders/Packages.php
+++ database/seeders/Packages.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
+
 use Faker\Factory as Faker;
 
 use app\Models\Project;
