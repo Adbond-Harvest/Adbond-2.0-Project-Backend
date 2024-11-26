2e022fbfed880aa7ee89b678512ef7988390a194
princeak <akalodave@gmail.com>
2024-11-26 16:12:07 +0100

    updated Save Media request

--- app/Http/Requests/SaveMedia.php
+++ app/Http/Requests/SaveMedia.php
@@ -25,8 +25,8 @@ class SaveMedia extends BaseRequest
     public function rules(): array
     {
         return [
-            // 'media' => ["required","file","mimes:jpeg,png,jpg,gif,x-ms-asf,x-flv,flv,video/x-m4v,video/mp4,x-mpegURL,MP2T,3gpp,quicktime,x-msvideo,x-ms-wmv,avi","max:50000", new VideoDuration(60)]
-            "media" => "mimes:jpg,jpeg,png,gif,webp,mp4,mpeg,mov,avi,wmv,webm,flv,3gp,m4v"
+            'media' => ["required","file","mimes:jpg,jpeg,png,gif,webp,mp4,mpeg,mov,avi,wmv,webm,flv,3gp,m4v","max:50000", new VideoDuration(60)]
+            // "media" => "mimes:jpg,jpeg,png,gif,webp,mp4,mpeg,mov,avi,wmv,webm,flv,3gp,m4v"
             // "media" => "required"
         ];
     }
