e9429ace531402f5e71d9254ce28b6537bf90f9f
Akachukwu Aneke <akalodave@gmail.com>
2025-02-24 11:49:34 +0100

    added search to transactions

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -27,6 +27,11 @@ class Offer extends Model
         return $this->belongsTo(Client::class);
     }
 
+    public function package()
+    {
+        return $this->belongsTo(Package::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
