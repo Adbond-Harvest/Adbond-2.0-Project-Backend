bd2cf7a2f388a3fe04b34009261193d288fc582b
Akachukwu Aneke <akalodave@gmail.com>
2025-06-23 18:16:21 +0100

    bug fix

--- app/Http/Controllers/Client/OfferController.php
+++ app/Http/Controllers/Client/OfferController.php
@@ -76,7 +76,7 @@ class OfferController extends Controller
             return Utilities::ok(new OfferResource($offer));
 
         }catch(\Exception $e){
-            return Utilities::error($e, 'An error occurred while trying to send verification mail, Please try again later or contact support');
+            return Utilities::error($e, 'An error occurred while trying to perform this operation, Please try again later or contact support');
         }
     }
 
