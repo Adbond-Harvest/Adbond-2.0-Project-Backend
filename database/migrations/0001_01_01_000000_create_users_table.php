101b0bc1a4dce337b92ca072e04e6264baf1d1ce
Akachukwu Aneke <akalodave@gmail.com>
2025-10-02 08:02:33 +0100

    more migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('title')->nullable();
             $table->string('firstname');
-            $table->string('lastname');
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('email_confirmed')->default(1);
